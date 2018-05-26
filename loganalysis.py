b48baa38788c71921021745318fe0dbbb55bbb37
sriram <sriramprasadtalasila@gmail.com>
2018-05-26 15:06:44 +0530

    Update loganalysis.py

--- loganalysis.py
+++ loganalysis.py
@@ -1,32 +1,35 @@
 import psycopg2
-#psycopg2 module help to connect with postgresql database
-#connection to the database news as user vagrant
-conn = psycopg2.connect(database="news", user = "vagrant", password = "12345", hos$cur = conn.cursor()
 
-#Function that prints Most popular articles all the time
+#connection to the database
+conn = psycopg2.connect(database="news", user = "vagrant", password = "12345", host = "127.0.0.1", port = "5432")
+cur = conn.cursor()
+
+#Function that prints Most popular articles all the time using view top_art
 def print_pop_art():
     print("Top three most popular articles of the time\n")
-    cur.execute("select count(path),REPLACE(path, '/article/','')from log where st$    rows = cur.fetchall()
+    cur.execute("select articles.title,top_art.count from articles INNER JOIN top_art ON articles.slug=top_art.replace order by top_art.count DESC;")
+    rows = cur.fetchall()
     for row in rows:
-        print  str(row[1]),"--- ",row[0], "\n"
+        print  str(row[0]),"--->",row[1], "\n"
 
 #This function uses views author_slug And slug count
 #to get the popular authors all the time
 # Refer to the readme for more information about views
 # and thier schemas 
-
 def print_pop_auth():
     print("Top most popular authors of the time\n")
-    cur.execute("select author_slug.name,sum(slug_count.count) from author_slug IN$    rows = cur.fetchall()
+    cur.execute("select author_slug.name,sum(slug_count.count) from author_slug INNER JOIN slug_count ON  author_slug.slug=slug_count.replace GROUP BY author_slug.name ORDER BY sum(slug_count.count) DESC;")
+    rows = cur.fetchall()
     for row in rows:
         print  str(row[0]),"--->",row[1], "\n"
 
 #this function prints the failure percentage of requests
 def print_err_percent():
-    print("Day with more than 1% of requests lead to errors\n")
-    cur.execute("select fail_count.count*100.00 / req_count.count as percent,fail_$    rows = cur.fetchall()
+    print("Day with more than 1\% of requests lead to errors\n")
+    cur.execute("select fail_count.count*100.00 / req_count.count as percent,fail_count.time from fail_count INNER JOIN req_count ON fail_count.time=req_count.time ORDER BY  fail_count.count*100.00 / req_count.count DESC LIMIT 1;")
+    rows = cur.fetchall()
     for row in rows:
-        print  row[1],"--->",round(row[0],2),"%","\n"
+        print  row[1],"--->",round(row[0],2),"%", "\n"
 
 print_pop_art()
 print_pop_auth()
